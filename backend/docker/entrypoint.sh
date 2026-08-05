e9b41a2012b7da4df7bea8663921cf09e54af40d
paolalisssv <157732009+paolalisssv@users.noreply.github.com>
2026-08-04 21:18:28 -0600

    Update entrypoint.sh

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -6,6 +6,12 @@ set -e
 sed -ri "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
 sed -ri "s/:80>/:${PORT}>/g" /etc/apache2/sites-available/000-default.conf
 
+if [ -n "${FIREBASE_CREDENTIALS_BASE64:-}" ]; then
+    mkdir -p storage/app/firebase
+    echo "${FIREBASE_CREDENTIALS_BASE64}" | base64 -d > storage/app/firebase/service-account.json
+    export FIREBASE_CREDENTIALS="$(pwd)/storage/app/firebase/service-account.json"
+fi
+
 if [ ! -L public/storage ]; then
     php artisan storage:link
 fi
